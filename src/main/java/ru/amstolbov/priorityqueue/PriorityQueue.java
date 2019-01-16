8058f565afae29b28849d458fed61b422e99713b
Alex <stolbov_alexey@mail.ru>
2019-01-16 22:09:21 +0300

    add priority queue

--- src/main/java/ru/amstolbov/priorityqueue/PriorityQueue.java
+++ src/main/java/ru/amstolbov/priorityqueue/PriorityQueue.java
@@ -1,4 +1,25 @@
 package ru.amstolbov.priorityqueue;
 
+import java.util.LinkedList;
+
+/**
+ * Очередь задач с приоритетом.
+ */
 public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+
+    /**
+     *Метод должен вставлять задачу в нужную позицию.
+     * Нужная позиция определяется приоритетом задачи.
+     * @param task задаче
+     */
+    public void put(Task task) {
+        tasks.stream().takeWhile(el -> el.getPriority() > task.getPriority()).findFirst().ifPresentOrElse(el -> tasks.add(tasks.indexOf(el), task),
+                                                                                                          () -> tasks.add(task));
+    }
+
+    public Task take() {
+        return this.tasks.poll();
+    }
+
 }
--- src/test/java/ru/amstolbov/priorityqueue/PriorityQueueTest.java
+++ src/test/java/ru/amstolbov/priorityqueue/PriorityQueueTest.java
@@ -1,7 +1,22 @@
 package ru.amstolbov.priorityqueue;
 
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class PriorityQueueTest {
+
+    @Test
+    public void whenHigherPriority() {
+        PriorityQueue priorityQueue = new PriorityQueue();
+        String nameOne = "One";
+        priorityQueue.put(new Task("Two", 2));
+        priorityQueue.put(new Task("Three", 3));
+        priorityQueue.put(new Task(nameOne, 1));
+        priorityQueue.put(new Task("Four", 4));
+        Task result = priorityQueue.take();
+        assertThat(result.getDesc(), is(nameOne));
+    }
 
 }
